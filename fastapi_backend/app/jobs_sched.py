97c4756eaa584ef74974956421d95052c86856ea
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2021-09-26 17:32:22 -0300

    Changes for last chunk of updates

--- fastapi_backend/app/jobs_sched.py
+++ fastapi_backend/app/jobs_sched.py
@@ -12,30 +12,34 @@ from apscheduler.events import EVENT_JOB_EXECUTED, EVENT_JOB_ERROR
 from .graphql_mutations import SyncProjectFromControl
 from .graphql_models import GetProjectInput
 
-info_queue = set()
-info_times = list()
+QUEUE_DATA = dict()
 
 SYNC_INTERVAL_UTC_TIME_START = 20 # (23 SCL)
 SYNC_INTERVAL_UTC_TIME_STOP = 4 # (7 SCL)
-DAYS_SINCE_LAST_SYNC_LIMIT = 7
+DAYS_SINCE_LAST_SYNC_LIMIT = 20
 
 def generate_updates_job():
     print('Starting generate_updates_job')
-    new_info_queue = set()
     dm.PlanParseFailedMessage.drop_collection()
     prods = dm.Project.objects(metadata__status='PRODUCTION', metadata__version='latest')
+    to_update = []
     for proj in prods:
         if proj.metadata.last_sync_date > (datetime.datetime.utcnow() - datetime.timedelta(days=DAYS_SINCE_LAST_SYNC_LIMIT)):
             print('Skipping update job for {}. Project.metadata.last_sync_date < DAYS_SINCE_LAST_SYNC_LIMIT'.format(proj.oid))
             continue
-        new_info_queue.add(proj.oid)
-    to_update = list(new_info_queue)
+        to_update.append(proj.oid)
     random.shuffle(to_update)
     for item in to_update:
         print('Added update job for {} to scheduler'.format(item))
         scheduler.update(item)
+        if item not in QUEUE_DATA:
+            QUEUE_DATA[item] = {
+                'num_retry': 0,
+                'time': 0,
+                'done': False
+            }
+        QUEUE_DATA[item]['num_retry'] = QUEUE_DATA[item]['num_retry'] + 1
     print('We have created {} jobs for project updates'.format(len(to_update)))
-    info_queue = new_info_queue
 
 def update_project_job(oid):
     print('Starting update for {}'.format(oid))
@@ -55,11 +59,11 @@ def update_project_job(oid):
     mut = SyncProjectFromControl()
     res = mut.mutate(None, None, data)
     end = time.time()
-    info_times.append(int(end - start))
-    if oid in info_queue:
-        info_queue.remove(oid)
     if res.code != 200:
         raise RuntimeError(res.message)
+    if oid in QUEUE_DATA:
+        QUEUE_DATA[oid]['time'] = int(end - start)
+        QUEUE_DATA[oid]['done'] = True
     print('Update for {} done'.format(oid))
 
 def listener(event):
@@ -67,9 +71,11 @@ def listener(event):
         print('[=>][!] Job [{}] has failed with the error: {}'.format(event.job_id, event.exception))
     else:
         print('[=>][+] Job [{}] has succeded'.format(event.job_id))
-    print('We have {} jobs in queue'.format(len(info_queue)))
-    if len(info_times) > 0 and len(info_queue) > 0:
-        print('The average time is {}s'.format(sum(info_times) / len(info_times)))
+    not_done = list(filter(lambda x: not x[1]['done'], QUEUE_DATA.items()))
+    done = list(filter(lambda x: x[1]['done'], QUEUE_DATA.items()))
+    avg = int(sum(map(lambda x: x[1]['time'], done)) / len(done))
+    print('We have {} projects to update'.format(len(not_done)))
+    print('The average time is {}s'.format(avg))
 
 def clock():
     print('The time is: {}'.format(datetime.datetime.now()))
@@ -89,7 +95,7 @@ class DACoTJobsScheduler:
         }
         self.__scheduler = AsyncIOScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults, timezone=timezone('America/Santiago'))
         self.__scheduler.add_listener(listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
-        self.__scheduler.add_job(generate_updates_job, trigger=CronTrigger.from_crontab('0 * * * *'), id='junction_updates_generator', replace_existing=True)
+        self.__scheduler.add_job(generate_updates_job, trigger=CronTrigger.from_crontab('*/5 * * * *'), id='junction_updates_generator', replace_existing=True)
         self.__scheduler.add_job(clock, trigger='interval', minutes=5, id='clock', replace_existing=True)
         self.info()
 
